c241767b2956cd9a0dad173da0078786b9a4b0de
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-05 13:12:51 +0300

    functions in-depth

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-// code your solution here
+
+//function 1
+function saturdayFun(activity = 'roller-skate') {
+   return `This Saturday, I want to ${activity}!`;
+}
+
+
+saturdayFun(); 
+saturdayFun("bathe my dog")
+
+// function 2
+const mondayWork = function (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+// console.log(mondayWork()); 
+// console.log(mondayWork("work from home"));
+
+mondayWork(); 
+mondayWork("work from home");
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
+// Test function
+let result1 = wrapAdjective('*');
+console.log(result1("a hard worker")); 
+
+let result2 = wrapAdjective('||');
+console.log(result2("a dedicated programmer")); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
